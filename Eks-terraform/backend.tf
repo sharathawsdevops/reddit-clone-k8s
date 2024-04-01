27eb9f18b8dfe9d75cc77f8604e8a81cc5daf0a0
sharath <sharathawsdevops@gmail.com>
2024-04-01 14:30:08 +0530

    EKS cluster code fix

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sharathdevops" # Replace with your actual S3 bucket name
+    bucket = "sharathdevops-mumbai" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
